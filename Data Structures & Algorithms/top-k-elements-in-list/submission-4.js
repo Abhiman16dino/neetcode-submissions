a0915a07081d23b11d3cc837e1ffa155dd94afb6
Abhiman16dino <73751573+Abhiman16dino@users.noreply.github.com>
2026-09-17 10:02:45 +0530

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const freq = {}
+        for(const num of nums) {
+            freq[num] = (freq[num] || 0) + 1
+        }
+
+        // console.log(freq)
+
+        const bucket = {}
+        for(const num in freq) {
+            const fr = freq[num]
+            if(!bucket[fr]){
+                bucket[fr] = []
+            }
+            bucket[fr].push(num)
+        }
+
+        // console.log(bucket)
+
+        const res = []
+        for(let i = nums.length+1; i > 0; i--){
+            if(bucket[i]){
+                while(bucket[i].length > 0){
+                    res.push(bucket[i].pop())
+                    if(res.length == k){
+                        return res
+                    }
+                }
+            }
+        }
+        return res
+    }
+}
